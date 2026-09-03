994a4af29985e2410070a1121056270a30b93fb5
Arjen Guis <guis.arjen@gmail.com>
2026-09-03 23:43:33 +0200

    fix(admin): col-md-.. aangepast naar col-lg-.. en  nieuwe col-md-.. sizes voor prettig schermgebruik

--- transcrypt/python/pages/page_admin.py
+++ transcrypt/python/pages/page_admin.py
@@ -54,8 +54,8 @@ class Settings(AccordionItem):
         input_enable_camera = E("input").attr("class", "form-control").attr("type", "checkbox")
         input_enable_logging = E("input").attr("class", "form-control").attr("type", "checkbox")
 
-        width_1 = "col-sm-5 col-md-3"
-        width_2 = "col-sm-3 col-md-2"
+        width_1 = "col-sm-5 col-md-4 col-lg-3"
+        width_2 = "col-sm-5 col-md-4 col-lg-2"
         self.body.append(
             E("div")
             .attr("class", "form-group row")
